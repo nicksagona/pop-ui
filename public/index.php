42dc213e1e78aca984650428169cf260fd2652cf
Nick Sagona <nick@nolainteractive.com>
2019-04-23 17:01:11 -0500

    Patch index.php file

--- public/index.php
+++ public/index.php
@@ -4,10 +4,10 @@ $autoloader = include __DIR__ . '/../vendor/autoload.php';
 
 try {
     $app = new Pop\Application($autoloader, include __DIR__ . '/../app/config/app.http.php');
-    $app->register(new Scroll\Module());
+    $app->register(new Pop\Ui\Module());
     $app->run();
 } catch (\Exception $exception) {
-    $app = new Scroll\Module();
+    $app = new Pop\Ui\Module();
     $app->httpError($exception);
 }
 

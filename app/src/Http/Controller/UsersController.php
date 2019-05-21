5c516ef8deff2e9077b55135e6bff614a8cc91dd
Nick Sagona <nick@nolainteractive.com>
2019-05-21 16:54:29 -0500

    Work on UI

--- app/src/Http/Controller/UsersController.php
+++ app/src/Http/Controller/UsersController.php
@@ -4,6 +4,7 @@ namespace Pop\Ui\Http\Controller;
 
 use Pop\Csv\Csv;
 use Pop\Http\Client\Stream;
+use Pop\Paginator\Paginator;
 
 class UsersController extends AbstractController
 {
@@ -17,6 +18,7 @@ class UsersController extends AbstractController
     {
         $this->prepareView('users.phtml');
 
+        $apiUrl = $this->application->config['api_url'];
         $filter = (null !== $this->request->getQuery('filter')) ? $this->request->getQuery('filter') : null;
         $fields = (null !== $this->request->getQuery('fields')) ? $this->request->getQuery('fields') : null;
 
@@ -35,13 +37,50 @@ class UsersController extends AbstractController
         }
 
         $this->view->title  = 'Users';
-        $this->view->apiUrl = $this->application->config['api_url'] . '/users';
+        $this->view->apiUrl = $apiUrl . '/users';
         $this->view->page   = (null !== $this->request->getQuery('page')) ? (int)$this->request->getQuery('page') : 1;
         $this->view->sort   = (null !== $this->request->getQuery('sort')) ? $this->request->getQuery('sort') : null;
         $this->view->filter = $filter;
         $this->view->fields = $fields;
+        $this->view->scroll = (null !== $this->request->getQuery('scroll'));
         $this->view->limit  = (null !== $this->request->getQuery('limit')) ?
             (int)$this->request->getQuery('limit') : $this->application->config['limit'];
+
+        if (null !== $this->request->getQuery('scroll')) {
+            $apiUrl .= '/users/count';
+            $query   = [];
+            $filter  = (null !== $this->request->getQuery('filter')) ? $this->request->getQuery('filter') : null;
+
+            if (null !== $filter) {
+                $query['filter'] = [];
+                if (is_array($filter)) {
+                    foreach ($filter as $f) {
+                        $query['filter'] = $f;
+                    }
+                } else {
+                    $query['filter'] = $filter;
+                }
+            }
+
+            if (!empty($query)) {
+                $apiUrl .= '?' . http_build_query($query);
+            }
+
+            $stream = new Stream($apiUrl);
+            $stream->send();
+
+            if (!empty($stream->getBody())) {
+                $users = json_decode($stream->getBody(), true);
+
+                $paginator = Paginator::createRange($users['results_count'], 10, 5);
+                $paginator->setClassOn('page-link')
+                    ->setClassOff('page-link');
+                $paginator->setSeparator(PHP_EOL . '        ');
+                $paginator->wrapLinks('li', 'page-item', 'page-item active');
+
+                $this->view->paginator = $paginator;
+            }
+        }
 
         $this->send();
     }

ac7c30650faf0ab27898048e184e58907226652b
Nick Sagona <nick@nolainteractive.com>
2019-05-22 10:58:43 -0500

    Add API service

--- app/src/Http/Controller/UsersController.php
+++ app/src/Http/Controller/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace Pop\Ui\Http\Controller;
 
+use Pop\Ui\Service\Api;
 use Pop\Csv\Csv;
 use Pop\Http\Client\Stream;
 use Pop\Paginator\Paginator;
@@ -18,19 +19,23 @@ class UsersController extends AbstractController
     {
         $this->prepareView('users.phtml');
 
-        $apiUrl = $this->application->config['api_url'];
-        $filter = (null !== $this->request->getQuery('filter')) ? $this->request->getQuery('filter') : null;
-        $fields = (null !== $this->request->getQuery('fields')) ? $this->request->getQuery('fields') : null;
-        $limit  = (null !== $this->request->getQuery('limit')) ?
+        $apiUrl        = $this->application->config['api_url'];
+        $page          = (null !== $this->request->getQuery('page')) ? (int)$this->request->getQuery('page') : 1;
+        $sort          = (null !== $this->request->getQuery('sort')) ? $this->request->getQuery('sort') : null;
+        $filter        = (null !== $this->request->getQuery('filter')) ? $this->request->getQuery('filter') : null;
+        $fields        = (null !== $this->request->getQuery('fields')) ? $this->request->getQuery('fields') : null;
+        $limit         = (null !== $this->request->getQuery('limit')) ?
             (int)$this->request->getQuery('limit') : $this->application->config['limit'];
+        $pageView      = (null !== $this->request->getQuery('scroll'));
+        $encodedFilter = null;
 
         if (null !== $filter) {
             if (is_array($filter)) {
                 foreach ($filter as $k => $v) {
-                    $filter[$k] = urlencode($v);
+                    $encodedFilter[$k] = urlencode($v);
                 }
             } else {
-                $filter = urlencode($filter);
+                $encodedFilter = urlencode($filter);
             }
         }
 
@@ -40,41 +45,31 @@ class UsersController extends AbstractController
 
         $this->view->title    = 'Users';
         $this->view->apiUrl   = $apiUrl . '/users';
-        $this->view->page     = (null !== $this->request->getQuery('page')) ? (int)$this->request->getQuery('page') : 1;
-        $this->view->sort     = (null !== $this->request->getQuery('sort')) ? $this->request->getQuery('sort') : null;
-        $this->view->filter   = $filter;
+        $this->view->page     = $page;
+        $this->view->sort     = $sort;
+        $this->view->filter   = $encodedFilter;
         $this->view->fields   = $fields;
-        $this->view->pageView = (null !== $this->request->getQuery('scroll'));
+        $this->view->pageView = $pageView;
         $this->view->limit    = $limit;
 
         if (null !== $this->request->getQuery('scroll')) {
-            $apiUrl .= '/users/count';
-            $query   = [];
-            $filter  = (null !== $this->request->getQuery('filter')) ? $this->request->getQuery('filter') : null;
+            $api = new Api(new Stream($apiUrl . '/users/count'));
 
             if (null !== $filter) {
-                $query['filter'] = [];
-                if (is_array($filter)) {
-                    foreach ($filter as $f) {
-                        $query['filter'] = $f;
-                    }
-                } else {
-                    $query['filter'] = $filter;
-                }
-                $apiUrl .= '?' . http_build_query($query);
+                $api->createQuery(null, $filter, null);
             } else if (!empty($this->request->getQuery('search_by')) && !empty($this->request->getQuery('search_for'))) {
-                $apiUrl .= '?filter[]=' . $this->request->getQuery('search_by') . '+LIKE+' . $this->request->getQuery('search_for') . '%25';
+                $query = 'filter[]=' . $this->request->getQuery('search_by') . '+LIKE+' .
+                    $this->request->getQuery('search_for') . '%25';
+                $api->appendQuery($query);
             }
 
-            $stream = new Stream($apiUrl);
-            $stream->send();
+            $api->send();
 
-            if (!empty($stream->getBody())) {
-                $users = json_decode($stream->getBody(), true);
-
+            if ($api->hasResponse()) {
+                $users     = $api->getResponse();
                 $paginator = Paginator::createRange($users['results_count'], $limit, 10);
                 $paginator->setClassOn('page-link')
-                    ->setClassOff('page-link');
+                          ->setClassOff('page-link');
                 $paginator->setSeparator(PHP_EOL . '        ');
                 $paginator->wrapLinks('li', 'page-item', 'page-item active');
 
@@ -96,36 +91,13 @@ class UsersController extends AbstractController
         $sort   = (null !== $this->request->getQuery('sort')) ? $this->request->getQuery('sort') : null;
         $filter = (null !== $this->request->getQuery('filter')) ? $this->request->getQuery('filter') : null;
         $fields = (null !== $this->request->getQuery('fields')) ? $this->request->getQuery('fields') : null;
-        $query  = [];
 
-        if (null !== $sort) {
-            $query['sort'] = $sort;
-        }
+        $api = new Api(new Stream($apiUrl));
+        $api->createQuery($sort, $filter, $fields);
+        $api->send();
 
-        if (null !== $filter) {
-            $query['filter'] = [];
-            if (is_array($filter)) {
-                foreach ($filter as $f) {
-                    $query['filter'] = $f;
-                }
-            } else {
-                $query['filter'] = $filter;
-            }
-        }
-
-        if (null !== $fields) {
-            $query['fields'] = (is_array($fields)) ? implode(',', $fields) : $fields;
-        }
-
-        if (!empty($query)) {
-            $apiUrl .= '?' . http_build_query($query);
-        }
-
-        $stream = new Stream($apiUrl);
-        $stream->send();
-
-        if (!empty($stream->getBody())) {
-            $users = json_decode($stream->getBody(), true);
+        if ($api->hasResponse()) {
+            $users = $api->getResponse();
             if (isset($users['results'])) {
                 Csv::outputDataToHttp($users['results'], [], 'users.csv');
             }
---
+++ app/src/Service/Api.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Pop\Ui\Service;
+
+use Pop\Http\Client\Stream;
+
+class Api
+{
+
+    /**
+     * Stream object
+     * @var Stream
+     */
+    protected $stream = null;
+
+    /**
+     * Constructor for the API service
+     *
+     * @param Stream $stream
+     */
+    public function __construct(Stream $stream)
+    {
+        $this->stream = $stream;
+    }
+
+    /**
+     * Method to get stream object
+     *
+     * return Stream
+     */
+    public function getStream()
+    {
+        return $this->stream;
+    }
+
+    /**
+     * Method to set query fields
+     *
+     * @param string $sort
+     * @param mixed  $filter
+     * @param string $fields
+     */
+    public function createQuery($sort = null, $filter = null, $fields = null)
+    {
+        $query = [];
+
+        if (null !== $sort) {
+            $query['sort'] = $sort;
+        }
+
+        if (null !== $filter) {
+            $query['filter'] = [];
+            if (is_array($filter)) {
+                foreach ($filter as $f) {
+                    $query['filter'] = $f;
+                }
+            } else {
+                $query['filter'] = $filter;
+            }
+        }
+
+        if (null !== $fields) {
+            $query['fields'] = (is_array($fields)) ? implode(',', $fields) : $fields;
+        }
+
+        if (!empty($query)) {
+            $this->stream->setFields($query);
+        }
+    }
+
+    /**
+     * Method to append query to URL
+     *
+     * @param string $query
+     * @return Api
+     */
+    public function appendQuery($query)
+    {
+        $url  = $this->stream->getUrl();
+        $url .= (strpos($url, '?') !== false) ? '&' . $query : '?' . $query;
+
+        $this->stream->setUrl($url);
+
+        return $this;
+    }
+
+    /**
+     * Method to send API call
+     *
+     */
+    public function send()
+    {
+        $this->stream->send();
+    }
+
+    /**
+     * Method to determine to response has body
+     *
+     * return boolean
+     */
+    public function hasResponse()
+    {
+        return !empty($this->stream->getBody());
+    }
+
+    /**
+     * Method to get response
+     *
+     * return array
+     */
+    public function getResponse()
+    {
+        return (!empty($this->stream->getBody())) ? json_decode($this->stream->getBody(), true) : [];
+    }
+
+
+}
